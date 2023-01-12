0f6125cd271d0826e2b53f4a58ac2617b8799cf2
Rubens Rodrigues <rubensr@viacast.tv>
2023-01-12 15:45:49 -0300

    change trigger for reload icons

--- server/drivers/viacast_lcd_utils.c
+++ server/drivers/viacast_lcd_utils.c
@@ -11,6 +11,9 @@ int filter(const struct dirent *name)
 void /* Display information from inotify_event structure */
 check_inotify_event(struct inotify_event *i, int *reload_icons)
 {
-  if (i->mask & IN_ALL_EVENTS)
+
+  if (i->mask & IN_MODIFY | IN_ATTRIB | IN_MOVED_FROM | IN_MOVED_TO |
+      IN_CREATE | IN_DELETE | IN_DELETE_SELF | IN_MOVE_SELF)
+
     *reload_icons = 1;
 }
